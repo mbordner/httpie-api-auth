3912835a9fe2fb332dfd8cd9db399858a612fcce
Michael Bordner <mbordner@gmail.com>
2020-06-17 11:25:51 -0400

    Update httpie_api_auth.py

--- httpie_api_auth.py
+++ httpie_api_auth.py
@@ -21,7 +21,7 @@ class ApiAuth:
         content_type = r.headers.get('content-type')
         if not content_type:
             if r.body != None and len(r.body) > 0:
-                content_type = 'application/json'
+                content_type = 'application/json'.encode('ascii')
             else:
                 content_type = ''
 
